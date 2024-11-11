8c7d3ea7c937a082452595600aef93616c63dca4
alexandermendes <alexanderhmendes@gmail.com>
2024-11-11 22:22:50 +0000

    feat: export stakingAbi and stakingConfig

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { useStaking } from './hooks/useStaking';
 export { StakingProvider } from './provider';
 export { type StakingContextType } from './context';
+export { stakingAbi } from './abis/staking';
+export { config as stakingConfig } from './config';
